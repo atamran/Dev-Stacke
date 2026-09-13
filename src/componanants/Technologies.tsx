347d58dea735d678a977944a2c6d6e247601bcd6
atamran <atmamran85@gmail.com>
2026-09-13 19:43:08 +0600

    2n-commit

--- src/componanants/Technologies.tsx
+++ src/componanants/Technologies.tsx
@@ -1,15 +1,24 @@
+import type { promises } from "dns";
+import { use } from "react";
+import type { ITechnologies } from "../types";
 
+interface ITechnologiesProps {
+    usersPromise: Promise<ITechnologies[]>;
+}
 
-
-const Technologies = () => {
+const Technologies = ({ usersPromise }: ITechnologiesProps) => {
+    console.log(usersPromise, "userPromise");
+    const data = use(usersPromise);
+    console.log(data, "data");
     return (
         <div>
             <div className='items-center justify-between gap-4 container mx-auto my-4'>
                 <h1 className='text-4xl font-bold'>Explore the <span className=" bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent "> Technologies</span></h1>
                 <p>Pick one technology per category to build your ideal stack.</p>
            <div className='grid grid-cols-12 gap-4'>
-            <div className='col-span-9 rounded-md shadow-2xl p-4 bg-red-800 h-[500px]'>
+            <div className='col-span-9 rounded-md shadow-2xl p-4'>
             
+           
                 
             </div>
             <div className='col-span-3 shadow-2xl py-4 p-4 '>
--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,20 @@ import Banner from './componanants/Banner'
 import Technologies from './componanants/Technologies'
 import Section from './componanants/Section'
 import Footer from './componanants/Footer'
+import type { ITechnologies } from './types'
 
-const usersFetch = async (): Promise<any[]> => {
+const usersFetch = async (): Promise<ITechnologies[]> => {
   const response = await fetch("/tech.json");
   const data = await response.json();
   return data;
 };
+const usersPromise =   usersFetch();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   <Navbar/>
   <Banner/>
-  <Technologies/>
+  <Technologies usersPromise={usersPromise} />
   <Section/>
   <Footer/>
   </StrictMode>,
